ba84519787408777dfaaaba244abb71683977185
klemen rahne <rahne.klemen@gmail.com>
2017-04-10 13:42:34 +0200

    simpletic integration

--- 03/maxwell.py
+++ 03/maxwell.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 #todo: ne deluje???!!!!!!
-lamda=0
 
+
+lamda=0
 T_l=1
 T_r=2
 n=20
@@ -15,17 +16,12 @@ def odvod(tabela):
     for i in range(n):
         tabela_odvod[i]=tabela[n+i]
 
-    ##odvod impulza
-    ##i=0,...n-1
-    ##n,..2*n-1
-
     tabela_odvod[n]=-1*(2*tabela[0]-tabela[1]+lamda*tabela[0]**3)
+    tabela_odvod[-1]=-1*(2*tabela[n-1]-tabela[n-2]+lamda*tabela[n-1]**3)
     # test[n] += 2
     for i in range(1,n-1):
         tabela_odvod[n+i]=-1*(3*tabela[i]-tabela[i-1]-tabela[i+1]+lamda * tabela[i]**3)
-        # test[n+i] +=2
 
-    tabela_odvod[2*n-1]=-1*(2*tabela[n-1]-tabela[n-2]+lamda*tabela[n-1]**3)
     # test[2*n-1] += 1
 
 
@@ -48,14 +44,13 @@ def rk4():
 
     y0 = np.zeros(2 * n )
     for i in range(n):
-        y0[n+i]=np.random.uniform()
-        y0[n]=np.random.uniform()
+        y0[n+i]=1
         # y0[n+i]=1
 
 
-    h=10**0
+    h=10**-2
     profil=[]
-    dolzina=10**5
+    dolzina=10**6
     print(dolzina,dolzina*0.95, dolzina-0.95*dolzina)
     for i in range(dolzina):
         if (i%1000==0 and i!=0):
@@ -63,12 +58,12 @@ def rk4():
         # if(i%dolzina*0.1==0):
         #     print(dolzina*0.1)
         # print(i)
-        s=y0
-        k1=odvod(s)
-        k2=odvod(s+h*k1/2)
-        k3=odvod(s+h*k2/2)
-        k4=odvod(s+h*k3)
-        y0=s+h*(k1 + k2 + k2 + k3 + k3 + k4) / 6
+
+        k1=odvod(y0)
+        k2=odvod(y0+h*k1/2)
+        k3=odvod(y0+h*k2/2)
+        k4=odvod(y0+h*k3)
+        y0=y0+h*(k1 + k2 + k2 + k3 + k3 + k4) / 6
 
         # if i> dolzina*0.95:
         #     profil.append(y0)
@@ -77,9 +72,10 @@ def rk4():
             # print(i)
             y0[n]=np.random.normal(loc=y0[n],scale=np.sqrt(T_l))
             y0[2*n-1]=np.random.normal(loc=y0[2*n-1],scale=np.sqrt(T_r))
+        #
+        # y0[n]=np.random.normal(loc=y0[n],scale=T_l)
+        # y0[2*n-1]=np.random.normal(loc=y0[2*n-1],scale=T_r)
         profil.append(y0)
-
-
     return profil
     # return y0
 
@@ -110,7 +106,7 @@ plt.plot(b)
 plt.grid()
 plt.show()
 
-# plt.plot(c)
+# plt.plot(a[-1])
 # plt.grid()
 # plt.show()
 
