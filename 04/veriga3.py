ce23fef794a6f6c61cf098bf27cd549e5dbc95ee
klemen rahne <rahne.klemen@gmail.com>
2017-04-23 19:07:15 +0200

    ne dela?!!!!!!

--- 04/veriga3.py
+++ 04/veriga3.py
@@ -5,28 +5,33 @@ import matplotlib.pyplot as plt
 def vektor(kot1,kot2):
     return np.cos(kot1)*np.sin(kot2), np.sin(kot1)*np.sin(kot2),np.cos(kot2)
 
-def stanje(n):
-    T=1
-    N=10**5
+def inv_vektor(x5,y5,z5):
+    return np.arccos(z5/(x5**2+y5**2+z5**2)),np.arctan(y5/x5)
+
+def stanje(n,T):
+    # T=1
+    N=10**4
     fi=np.random.rand(n)*2*np.pi
     theta=np.arccos(2*np.random.rand(n)-1)
     rand_st=np.random.rand(N)
+    print(fi,theta)
     for i in range(N):
         j=np.random.randint(0,n-1)
 
         x1,y1,z1=vektor(fi[j],theta[j])
         x2,y2,z2=vektor(fi[j+1],theta[j+1])
-
+        print('tocke', x1,x2,'\t',y1,y2,'\t',z1,z2,'\t',j)
         x_sum=x1+x2
         y_sum=y1+y2
         z_sum=z1+z2
 
         #presecisce-krona med sfero (r=1 s=0) in sfero (r=1 in s=rs)
         rs_kvadrat=x_sum**2+y_sum**2+z_sum**2
+        print('rs',rs_kvadrat)
         rs=np.sqrt(rs_kvadrat)
 
         #kota krone
-        alfa=np.arcos(rs_kvadrat/2)
+        alfa=np.arccos(rs/2)
         beta=np.random.rand()*2*np.pi
 
         #delta vektor
@@ -47,7 +52,23 @@ def stanje(n):
         y2_nov=y_sum-y1_nov
         z2_nov=z_sum-z1_nov
 
-        #todo spremema energije
+        #energije
+        stara=-(x1*x2+y1*y2+z1*z2)
+        nova=(x1_nov*x2_nov+y1_nov*y2_nov+z1_nov*z2_nov)
+        delta=nova-stara
+
+
+        if rand_st[i]< np.exp(-delta/T):
+            print('-------------------------------^')
+            a,b=inv_vektor(x1_nov,y1_nov,z1_nov)
+            theta[j]=a
+            fi[j]=b
+
+            c,d=inv_vektor(x2_nov,y2_nov,z2_nov)
+            theta[j+1]=c
+            fi[j+1]=d
+
+
     return fi, theta
 
 def korelacija_single(phi,theta):
@@ -59,17 +80,19 @@ def korelacija_single(phi,theta):
     return kor
 
 
-def korelacija():
+def korelacija(temperatura):
     M=30
     korel_dolzina=np.zeros(M)
     for iteracija in range(10**2):
-        a,b=stanje(M)
+        a,b=stanje(M,temperatura)
         korel_dolzina+=korelacija_single(a,b)
         print(iteracija)
 
     return korel_dolzina
 
-plt.plot(korelacija())
+
+T1=1
+plt.plot(korelacija(T1))
 plt.grid()
 plt.show()
 
